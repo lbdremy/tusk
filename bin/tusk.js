e66ec6f72e5da557e252f85d17e334850fc305b9
Remy Loubradou <remyloubradou@gmail.com>
2013-03-15 20:58:15 +0100

    Use README.md, package.json.
    Close #8
    Close #9
    Close #5 (highlight.js)

--- bin/tusk.js
+++ bin/tusk.js
@@ -9,27 +9,28 @@ var tusk = require('./../'),
 
 // Build the command line tool and parse the arguments
 var argv = optimist
-	.usage('Usage: $0 [--input=FOLDER] [--output=FOLDER] [options]')
+	.usage('Usage: $0 [--input=FOLDER] [options]')
 	.describe('input','use the files contained in FOLDER to generate the documentation')
-	.describe('output','save the documentation in FOLDER')
-    .describe('layout','use the FILE or FOLDER as the layout of the site')
-    .describe('style','use the FILE as the css file of the site')
+    .describe('styles','use the FILE(s) as the css file of the site')
     .describe('scripts','use the FILE(s) as the javascript script(s) of the site')
     .describe('help','show the help')
-    .alias('script','scripts')
     .alias('i','input')
     .alias('o','output')
     .alias('h','help')
     .boolean('help')
     .default('input',process.cwd())
-    .default('output',process.cwd() + '/documentation')
-    .default('layout','tusk/lib/views/index.jade')
-    .default('style','tusk/lib/assets/stylesheets/style.css')
-    .default('scripts','tusk/lib/assets/scripts/script.js')
+    .default('styles','')
+    .default('scripts','')
     .argv;
 
 // Show the help
-if(argv._.length === 0 || argv.help){
-    console.log(optimist.help());
+if(argv.help){
+    process.stdout.write(optimist.help());
     process.exit(0);
 }
+
+// Document
+tusk.document(argv,function(err,page){
+    if(err) throw err;
+    console.log(page);
+});
